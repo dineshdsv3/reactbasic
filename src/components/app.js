fb6f5c39a37ac4429b29ca8f9b3a05f349f3e579
dineshdsv3 <dineshdsv3@gmail.com>
2020-02-08 18:50:08 +0530

    Added componentDidMount

--- src/components/app.js
+++ src/components/app.js
@@ -1,49 +1,50 @@
 import React from "react";
 import GamesList from "./gamesList";
 
-const games = undefined;
-// const games = [
-//   {
-//     _id: 1,
-//     price: 32.99,
-//     thumbnail: "https://wallpapercave.com/wp/wp1826564.jpg",
-//     header: "Age of Empire",
-//     players: "2-3",
-//     duration: "30"
-//   },
-//   {
-//     _id: 2,
-//     price: 10.99,
-//     thumbnail: "https://wallpapercave.com/wp/wp2208672.jpg",
-//     header: "Pubg",
-//     players: "3-4",
-//     duration: "60"
-//   },
-//   {
-//     _id: 3,
-//     price: 11.99,
-//     thumbnail: "https://wallpapercave.com/wp/k6fdpmv.jpg",
-//     header: "NFS Most Wanted",
-//     players: "4-6",
-//     duration: "40"
-//   }
-// ];
+// const games = undefined;
+const games = [
+  {
+    _id: 1,
+    price: 32.99,
+    thumbnail: "https://wallpapercave.com/wp/wp1826564.jpg",
+    header: "Age of Empire",
+    players: "2-3",
+    duration: "30"
+  },
+  {
+    _id: 2,
+    price: 10.99,
+    thumbnail: "https://wallpapercave.com/wp/wp2208672.jpg",
+    header: "Pubg",
+    players: "3-4",
+    duration: "60"
+  },
+  {
+    _id: 3,
+    price: 11.99,
+    thumbnail: "https://wallpapercave.com/wp/k6fdpmv.jpg",
+    header: "NFS Most Wanted",
+    players: "4-6",
+    duration: "40"
+  }
+];
 
-// const App = () => (
-  
-// );
 
 
 class App extends React.Component {
   state = {
     games:[]
   }
+
+  componentDidMount() {
+    this.setState( {games: games})
+  }
   
   render() {
 
     return(
       <div className="ui container">
-    <GamesList games={games} />
+    <GamesList games={this.state.games} />
   </div>
     
   )
