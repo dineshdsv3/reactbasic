068424ed806a8ff806e159668c2b1e8c7bd366a1
dineshdsv3 <dineshdsv3@gmail.com>
2020-02-11 03:25:16 +0530

    Added checkbox for featured property

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,33 +1,100 @@
-import React from 'react';
+import React from "react";
 
-class GameForm extends React.Component{
-    state = {
-        name: ""
-    }
-    
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(this.state)
-    }
+class GameForm extends React.Component {
+  state = {
+    name: "",
+    description: "",
+    price: 0,
+    duration: 0,
+    players: "",
+    featured: true
+  };
 
-    handleChange = (e) => {
-        this.setState({[e.target.name]:e.target.value})
-    }
-    render() {
-        return (
-           <form className ="ui form" onSubmit={this.handleSubmit}>
-               <div className= "field">
-                   <label>Name</label> 
-                   <input type="text" id="name" placeholder="Game name" name="name" onChange={this.handleChange}/>
-               </div>
-               <div className= "field">
-                   <label>Description</label> 
-                   <textarea type="text" id="name" placeholder="Game description" name="description" onChange={this.handleChange}/>
-               </div>
-               <button className="ui button" type="submit">Submit</button>
-           </form>
-        )
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]:
+        e.target.type === "number" ? parseInt(e.target.value) : e.target.value
+    });
+  };
+
+  handleCheckChange = e => {
+    this.setState({ [e.target.name]: e.target.checked });
+  };
+  render() {
+    return (
+      <form className="ui form" onSubmit={this.handleSubmit}>
+        <div className="field">
+          <label>Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Game name"
+            name="name"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="field">
+          <label>Description</label>
+          <textarea
+            type="text"
+            id="name"
+            placeholder="Game description"
+            name="description"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="three fields">
+          <div className="field">
+            <label>Price</label>
+            <input
+              type="number"
+              id="price"
+              placeholder="Price"
+              name="price"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="field">
+            <label>Duration</label>
+            <input
+              type="number"
+              id="duration"
+              placeholder="Duration"
+              name="duration"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="field">
+            <label>Players</label>
+            <input
+              type="text"
+              id="text"
+              placeholder="players"
+              name="players"
+              onChange={this.handleChange}
+            />
+          </div>
+        </div>
+        <div className="inline field">
+          <input
+            type="checkbox"
+            checked={this.state.featured}
+            name="featured"
+            onChange={this.handleCheckChange}
+          />
+          <label>featured?</label>
+        </div>
+        <button className="ui button" type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
 
 export default GameForm;
